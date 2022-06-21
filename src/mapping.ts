de62fe24cabd85c518c2082d1bc9c95d6d9ac8d3
Frank <frank.pfeift@protonmail.com>
2022-06-21 12:44:26 +0200

    added handler from old subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,103 +1,230 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
 import {
   Contract,
-  AuctionCancelled,
   Auction_BidPlaced,
   Auction_BidRemoved,
   Auction_EndTimeUpdated,
   Auction_IncentivePaid,
   Auction_Initialized,
-  Auction_ItemClaimed,
   Auction_StartTimeUpdated,
-  Contract_BiddingAllowed
+  Contract_BiddingAllowed,
+  Auction_ItemClaimed,
+  AuctionCancelled,
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import {
+  Auction,
+  Bid,
+  Statistic,
+  Incentive,
+  User,
+  Contract as ContractEntity
+} from "../generated/schema";
+import { getOrCreateBid, getOrCreateUser } from "./helper";
 
-export function handleAuctionCancelled(event: AuctionCancelled): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleAuction_BidPlaced(event: Auction_BidPlaced): void {
+  //Auction
+  let auction = Auction.load(event.params._auctionID.toString())!;
+  let bid = getOrCreateBid(event.params._bidder, event.params._bidAmount, auction as Auction, event);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  //Set new bid
+  auction.totalBids = auction.totalBids.plus(BigInt.fromI32(1));
+  auction.lastBidTime = event.block.timestamp;
+  auction.highestBid = event.params._bidAmount;
+  auction.highestBidder = event.params._bidder;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //Update user
+  let user = getOrCreateUser(event.params._bidder);
+  user.bids = user.bids.plus(BigInt.fromI32(1));
+  user.bidAmount = user.bidAmount.plus(event.params._bidAmount);
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity._auctionId = event.params._auctionId
-  entity._tokenId = event.params._tokenId
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.checkIndex(...)
-  // - contract.checkPubKey(...)
-  // - contract.createAuction(...)
-  // - contract.getAuctionBidDecimals(...)
-  // - contract.getAuctionBidMultiplier(...)
-  // - contract.getAuctionDebt(...)
-  // - contract.getAuctionDueIncentives(...)
-  // - contract.getAuctionEndTime(...)
-  // - contract.getAuctionHammerTimeDuration(...)
-  // - contract.getAuctionHighestBid(...)
-  // - contract.getAuctionHighestBidder(...)
-  // - contract.getAuctionIncMax(...)
-  // - contract.getAuctionIncMin(...)
-  // - contract.getAuctionInfo(...)
-  // - contract.getAuctionPresets(...)
-  // - contract.getAuctionStartTime(...)
-  // - contract.getAuctionStepMin(...)
-  // - contract.getContractAddress(...)
-  // - contract.getTokenId(...)
-  // - contract.getTokenKind(...)
-  // - contract.isBiddingAllowed(...)
-  // - contract.onERC1155BatchReceived(...)
-  // - contract.onERC1155Received(...)
-  // - contract.onERC721Received(...)
+  user.save();
+  auction.save();
+  bid.save();
 }
 
-export function handleAuction_BidPlaced(event: Auction_BidPlaced): void {}
+export function handleAuction_BidRemoved(event: Auction_BidRemoved): void {
+  let auction = Auction.load(event.params._auctionID.toString());
+  let entity = getOrCreateBid(event.params._bidder, event.params._bidAmount, auction as Auction, event);
 
-export function handleAuction_BidRemoved(event: Auction_BidRemoved): void {}
+  //Update user
+  let user = getOrCreateUser(event.params._bidder);
+  user.outbids = user.outbids.plus(BigInt.fromI32(1));
+
+  entity.outbid = true;
+  entity.save();
+  user.save();
+}
 
 export function handleAuction_EndTimeUpdated(
   event: Auction_EndTimeUpdated
-): void {}
+): void {
+  let entity = Auction.load(event.params._auctionID.toString())!;
+  entity.endTime = event.params._endTime;
+  entity.save();
+}
 
 export function handleAuction_IncentivePaid(
   event: Auction_IncentivePaid
-): void {}
+): void {
+  let incentiveId =
+    event.params._auctionID.toString() +
+    "_" +
+    event.params._earner.toHexString() +
+    "_" +
+    event.params._incentiveAmount.toString();
 
-export function handleAuction_Initialized(event: Auction_Initialized): void {}
+  let incentive = Incentive.load(incentiveId);
 
-export function handleAuction_ItemClaimed(event: Auction_ItemClaimed): void {}
+  if (incentive == null) {
+    incentive = new Incentive(incentiveId);
+  }
+
+  incentive.amount = event.params._incentiveAmount;
+  incentive.earner = event.params._earner;
+  incentive.auctionID = event.params._auctionID;
+  incentive.receiveTime = event.block.timestamp;
+
+  let auction = Auction.load(event.params._auctionID.toString())!;
+  incentive.tokenIndex = auction.tokenIndex;
+  incentive.tokenId = auction.tokenId;
+  incentive.contractAddress = auction.contractAddress;
+  incentive.type = auction.type;
+  incentive.auctionOrderId = auction.orderId;
+  // @todo: Payouts object: previousBidder
+
+
+  let user = getOrCreateUser(event.params._earner);
+  user.payouts = user.payouts.plus(BigInt.fromI32(1));
+  user.payoutAmount = user.payoutAmount.plus(event.params._incentiveAmount);
+
+  user.save();
+  incentive.save();
+}
+
+export function handleAuction_Initialized(event: Auction_Initialized): void {
+  //Update statistics
+  let statistics = Statistic.load("0");
+  if (statistics == null) {
+    statistics = new Statistic("0");
+    statistics.erc721Auctions = BigInt.fromI32(0);
+    statistics.erc1155Auctions = BigInt.fromI32(0);
+  }
+
+  let orderId = BigInt.fromI32(0);
+  let type =
+    event.params._tokenKind.toHexString() == "0x973bb640"
+      ? "erc1155"
+      : "erc721";
+
+  if (type == "erc1155") {
+    statistics.erc1155Auctions = statistics.erc1155Auctions.plus(
+      BigInt.fromI32(1)
+    );
+    orderId = statistics.erc1155Auctions;
+  } else {
+    statistics.erc721Auctions = statistics.erc721Auctions.plus(
+      BigInt.fromI32(1)
+    );
+    orderId = statistics.erc721Auctions;
+  }
+  statistics.save();
+
+  let auction = Auction.load(event.params._auctionID.toString());
+
+  if (auction == null) {
+    auction = new Auction(event.params._auctionID.toString());
+  }
+
+  auction.orderId = orderId;
+  auction.type = type;
+  auction.contractAddress = event.params._contractAddress;
+  auction.tokenId = event.params._tokenID;
+  auction.totalBids = BigInt.fromI32(0);
+  auction.claimed = false;
+  auction.lastBidTime = BigInt.fromI32(0);
+  auction.highestBid = BigInt.fromI32(0);
+  auction.highestBidder = Address.fromString(
+    "0x0000000000000000000000000000000000000000"
+  );
+
+  //Fetch auction info from contract
+  let contract = Contract.bind(event.address);
+
+  let result = contract.try_getAuctionInfo(event.params._auctionID);
+
+  // @todo: seller, createdAt, startsAt, endsAt, claimAt, 
+  //  contractId quantity, presetId, cancelled, ercType, bids objects
+  if (!result.reverted) {
+    let auctionInfo = result.value;
+    auctionInfo.auctionDebt
+    auctionInfo.biddingAllowed
+    auction.claimed = auctionInfo.claimed
+
+    let presets = auctionInfo.presets;
+    auction.bidDecimals = presets.bidDecimals
+    auction.bidMultiplier = presets.bidMultiplier
+    auction.hammerTimeDuration = BigInt.fromI32(presets.hammerTimeDuration)
+    auction.incMax = presets.incMax
+    auction.incMin = presets.incMin
+    auction.stepMin = presets.stepMin
+    auction.seller = auctionInfo.owner;
+    auction.createdAt = event.block.timestamp;
+    // auction.endsAt = 
+    // auction.claimAt = 
+    auction.highestBidder = auctionInfo.highestBidder;
+
+  }
+
+  auction.save();
+
+  let contractEntity = ContractEntity.load(
+    event.params._contractAddress.toHexString()
+  );
+  if (contractEntity == null) {
+    contractEntity = new ContractEntity(event.params._contractAddress.toHexString());
+    contractEntity.biddingAllowed = false;
+  }
+
+  contractEntity.save();
+}
 
 export function handleAuction_StartTimeUpdated(
   event: Auction_StartTimeUpdated
-): void {}
+): void {
+  let entity = Auction.load(event.params._auctionID.toString())!;
+  entity.startTime = event.params._startTime;
+  entity.save();
+}
 
 export function handleContract_BiddingAllowed(
   event: Contract_BiddingAllowed
-): void {}
+): void {
+  let entity = ContractEntity.load(event.params._contract.toHexString());
+  if (entity == null) {
+    entity = new ContractEntity(event.params._contract.toHexString());
+  }
+  entity.biddingAllowed = event.params._biddingAllowed;
+  entity.save();
+}
+
+export function handleAuction_ItemClaimed(event: Auction_ItemClaimed): void {
+  let auction = Auction.load(event.params._auctionID.toString())!;
+  auction.claimed = true;
+
+  let bid = getOrCreateBid(auction.highestBidder, auction.highestBid, auction as Auction, event);
+  bid.claimed = true;
+
+  let user = getOrCreateUser(auction.highestBidder);
+  user.wins = user.wins.plus(BigInt.fromI32(1));
+
+  user.save();
+
+  bid.save();
+  auction.save();
+}
+
+export function handleAuctionCancelled(event: AuctionCancelled): void {
+  let auction = Auction.load(event.params._auctionId.toString())!;
+  auction.cancelled = true;
+  auction.save();
+}
