8a59025a971f81cdd63c8e637c62f87f1c53834e
Frank <frank.pfeift@protonmail.com>
2022-12-21 12:01:58 +0100

    added constant cancellation time duration

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,5 @@ import { BigInt } from "@graphprotocol/graph-ts";
 
 export let BIGINT_ZERO = BigInt.fromI32(0);
 export let BIGINT_ONE = BigInt.fromI32(1);
+
+export let BIGINT_CANCELLATION_PERIOD_IN_SECONDS = BigInt.fromI32(3600);
