b8b1389df7f84c7ba980bd97f67032606626d4b8
Frank <frank.pfeift@protonmail.com>
2023-04-26 08:17:37 +0200

    feat: added category to auctions

--- src/mapping.ts
+++ src/mapping.ts
@@ -314,6 +314,7 @@ export function handleAuction_Initialized(
     if (!result.reverted && !resultHammerTime.reverted) {
         let auctionInfo = result.value;
 
+        auction.category = auctionInfo.info.category;
         auction.auctionDebt = auctionInfo.auctionDebt;
         auctionInfo.biddingAllowed;
         auction.claimed = auctionInfo.claimed;
