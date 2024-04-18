ea6e31b441eb4ab0814d8e70b958e1747763d7dc
orionstardust <justinono1990@outlook.com>
2024-04-18 06:33:34 -0400

    chore: update block info

--- src/constants.ts
+++ src/constants.ts
@@ -6,4 +6,4 @@ export let BIGINT_ONE = BigInt.fromI32(1);
 export let BIGINT_CANCELLATION_PERIOD_IN_SECONDS = BigInt.fromI32(3600);
 
 // TODO: Needs to be updated to activated block number
-export let BLOCK_NR_BUY_NOW_ACTIVATED = BigInt.fromI32(54989367);
+export let BLOCK_NR_BUY_NOW_ACTIVATED = BigInt.fromI32(55931248);
